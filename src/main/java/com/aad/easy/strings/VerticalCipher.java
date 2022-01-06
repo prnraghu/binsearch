1387c9f8e9f51c7380791b78df99dd45c16ac3ca
abira369 <abi.nit69@gmail.com>
2022-01-05 18:13:56 -0800

    Vertical Cipher

---
+++ src/main/java/com/aad/easy/strings/VerticalCipher.java
@@ -0,0 +1,27 @@
+package com.aad.easy.strings;
+
+/*Vertical Cipher
+        Easy
+
+        Given a string s and an integer k,
+        rearrange s into k rows so that s can be read vertically (top-down, left to right).
+
+        Constraints
+
+        n ≤ 10,000 where n is the length of s
+        k ≤ 1,000*/
+
+public class VerticalCipher {
+
+    public String[] solve(String s, int k) {
+        String[] result = new String[k];
+        for(int i = 0; i < s.length() ; i++){
+            if(result[i%k] == null) {
+                result[i%k] = ""+s.charAt(i);
+            } else {
+                result[i%k] = result[i%k] + s.charAt(i);
+            }
+        }
+        return result;
+    }
+}
